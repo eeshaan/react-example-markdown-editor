3c4455038e9ca6dae1fa88305d905691fb5c3356
eeshaan <me@eeshaan.life>
2020-07-26 16:03:33 -0500

    import chakra and apply basic styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import marked from "marked";
 import DOMPurify from "dompurify";
+import { ThemeProvider, SimpleGrid, Box } from "@chakra-ui/core";
 
 import "./App.css";
 
@@ -12,14 +13,23 @@ function App() {
   }
 
   return (
-    <div className="editable">
-      <textarea onChange={update} value={markdown} />
-
-      <div
-        className="rendered"
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked(markdown)) }}
-      />
-    </div>
+    <ThemeProvider>
+      {
+        <SimpleGrid minChildWidth="120px" colummns={2}>
+          <Box>
+            <div className="editable">
+              <textarea onChange={update} value={markdown} />
+            </div>
+          </Box>
+          <Box bg="gray.100">
+            <div
+              className="rendered"
+              dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked(markdown)) }}
+            />
+          </Box>
+        </SimpleGrid>
+      }
+    </ThemeProvider>
   );
 }
 
